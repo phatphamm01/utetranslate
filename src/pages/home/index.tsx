14851cd7a923bb3a1a48b038855c18a5664c6f59
phatphamm01 <84543546+phatphamm01@users.noreply.github.com>
2021-11-23 15:13:58 +0700

    update title page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from "next";
 import { Fragment } from "react";
 import Home from "@containers/Home";
+import MetaTitle from "@design/MetaTitle";
 
 const Homepage: NextPage = () => {
   return (
     <Fragment>
+      <MetaTitle title="UTE Translate" />
       <Home />
     </Fragment>
   );
